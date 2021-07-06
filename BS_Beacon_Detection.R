f8499f2ac3d1abe2168d6ce6ecba4584616b32c8
MrEtmAr <72963769+MrEtmAr@users.noreply.github.com>
2021-07-06 11:00:15 +0300

    function update
    several additional functions for BS_Beacon analysis
    additional warning in smoothing filters

--- BS_Beacon_Detection.R
+++ BS_Beacon_Detection.R
@@ -6,8 +6,8 @@ require(RSQLite)
 require(gridExtra)
 
 
-Det_Data_from_ATLAS_server <- function(Query_Time_df,FullTag,BS=NA, SYS="Harod")
-{
+Det_Data_from_ATLAS_server <- function(Query_Time_df,FullTag,BS=NA, SYS="Harod",Fields='TAG, TIME, BS, RSSI, GAIN, SNR, SAMPLES_CLK')
+      {
    if (SYS=="Harod") #connects to Harod server
    {
       dbc <- dbConnect(RMySQL::MySQL(),
@@ -27,12 +27,12 @@ Det_Data_from_ATLAS_server <- function(Query_Time_df,FullTag,BS=NA, SYS="Harod")
       for (i in 1:length(FullTag)) 
       { # build a  DETECTIONS query for the system, the results include the variables listed below
          if (is.na(BS)) {
-            Q$query[(j-1)*Ntag+i] <- paste('select TAG,TIME,BS,RSSI,GAIN,SNR from DETECTIONS WHERE TAG=',FullTag[i],
+            Q$query[(j-1)*Ntag+i] <- paste('select',Fields,'from DETECTIONS WHERE TAG=',FullTag[i],
                                            'AND TIME >=', Query_Time_df$Start[j]*1000, 'AND TIME <=', Query_Time_df$End[j]*1000)
          #    query = paste('select TAG,TIME,BS,RSSI,GAIN,SNR from DETECTIONS WHERE TAG=',FullTag[i],
          #                               'AND TIME >=', Query_Time_df$Start[j]*1000, 'AND TIME <=', Query_Time_df$End[j]*1000)
          } else
-         Q$query[(j-1)*Ntag+i] <- paste('select TAG,TIME,BS,RSSI,GAIN,SNR from DETECTIONS WHERE TAG=',FullTag[i],
+         Q$query[(j-1)*Ntag+i] <-    paste('select',Fields,'from DETECTIONS WHERE TAG=',FullTag[i],
                                         'AND TIME >=', Query_Time_df$Start[j]*1000, 'AND TIME <=', Query_Time_df$End[j]*1000,'AND BS =', BS)
          # query = paste('select TAG,TIME,BS,RSSI,GAIN,SNR from DETECTIONS WHERE TAG=',FullTag[i],
          #               'AND TIME >=', Query_Time_df$Start[j]*1000, 'AND TIME <=', Query_Time_df$End[j]*1000, 'AND BS =', BS)
@@ -45,7 +45,6 @@ Det_Data_from_ATLAS_server <- function(Query_Time_df,FullTag,BS=NA, SYS="Harod")
    RawDet0 <- do.call(rbind.data.frame, All_Data)
    return(RawDet0)
 }
-
 Query_time_df <- function(Start_Time_Str,End_Time_Str,sample_timesPerDay=24,sample_lengthMinute=1)
       {
       num_Start_Time<-as.numeric(as.POSIXct(Start_Time_Str,
@@ -58,15 +57,17 @@ Query_time_df <- function(Start_Time_Str,End_Time_Str,sample_timesPerDay=24,samp
       Query_time$posEnd <- as.POSIXct(Query_time$End, tz="UTC", origin="1970-01-01")
       return(Query_time) 
       }
-Get_ATLAS_Det_DATA <- function(Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag,BS=NA)
+Get_ATLAS_Det_DATA <- function(Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag,BS=NA,slice=T,Fields='TAG,TIME,BS,RSSI,GAIN,SNR,SAMPLES_CLK')
       {
+      STime <- Sys.time()
       Query_Time <- Query_time_df(Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute)
-      RawDet0 <- Det_Data_from_ATLAS_server(Query_Time,FullTag,BS)
+      RawDet0 <- Det_Data_from_ATLAS_server(Query_Time,FullTag,BS,Fields = Fields)
       RawDet0$BS<-gsub("9720060", '', RawDet0$BS)
       RawDet0$TAG<-gsub("972006000", '', RawDet0$TAG)
       
       RawDet0$dateTime<-as.POSIXct((RawDet0$TIME)/1000, tz="UTC", origin="1970-01-01")
-      RawDet1 <- RawDet0 %>% mutate(M=minute(RawDet0$dateTime),H=hour(RawDet0$dateTime),D=date(RawDet0$dateTime)) %>% 
+    if (slice)  
+       RawDet1 <- RawDet0 %>% mutate(M=minute(RawDet0$dateTime),H=hour(RawDet0$dateTime),D=date(RawDet0$dateTime)) %>% 
          group_by(TAG,BS) %>%
          mutate(Tag_BS=factor(paste(BS,"-",TAG ))) %>% 
          add_count()%>%
@@ -74,6 +75,11 @@ Get_ATLAS_Det_DATA <- function(Start_Time_Str,End_Time_Str,sample_timesPerDay,sa
          group_by(M,H,D,TAG,BS) %>% 
          slice_head() %>% 
          ungroup()
+      else
+         RawDet1 <- RawDet0
+      
+      print('The time to download and reshpe data was')
+      print( Sys.time()-STime)
       return(RawDet1)
       }
 plotCountDet <- function(RawDet1,xAxisRes='3 hours')
@@ -137,13 +143,118 @@ plotBSperformance <- function(RawDet1,BS2plot,TAG2plot=NA ,xAxisRes='12 hours')
    grid.arrange(p1,p2,p3, nrow = 3)
    
 }
+plotTagsCount <- function (RawDet1,xAxisRes='3 hours',BS2plot=NA) #(RawDet1,BS2plot,TAG2plot=NA ,xAxisRes='12 hours')
+      {
+   A <- RawDet1 %>% 
+      group_by(H,M,D,BS) %>%
+      summarise(uniqueTagsDetected=length(unique(TAG)),dateTime=min(dateTime),
+                meanSNR=mean(SNR),meanRSSI=mean(RSSI)) %>%
+      arrange(dateTime) %>% 
+      ungroup()
+   if(any(!is.na(BS2plot)))
+      A <- A %>% filter(BS %in% BS2plot)
+   
+   p1 <-   A %>% ggplot(aes(dateTime, uniqueTagsDetected))+
+      geom_point(aes(x=dateTime,y= uniqueTagsDetected,group=BS, color=BS) )+
+      geom_line(aes(x=dateTime,y= uniqueTagsDetected,group=BS, color=BS) )+
+      scale_x_datetime(date_breaks = xAxisRes)+
+      theme(axis.text.x=element_text(angle=60, hjust=1)) +
+      theme(axis.text.x=element_blank()) +
+      labs(title= paste('number unique Tags '),
+           subtitle = paste('period= ',RawDet1$dateTime[1],' - ',max(RawDet1$dateTime)),
+           x = "", 
+           y = "number of unique tags")
+   
+   p2   <-   A %>% ggplot(aes(dateTime, meanSNR))+
+      geom_point(aes(x=dateTime,y= meanSNR,group=BS, color=BS) )+
+      geom_line(aes(x=dateTime,y= meanSNR,group=BS, color=BS) )+
+      scale_x_datetime(date_breaks = xAxisRes)+
+      theme(axis.text.x=element_text(angle=60, hjust=1)) +
+      labs(title= paste('mean SNR of detections'),
+           subtitle = paste('period= ',RawDet1$dateTime[1],' - ',max(RawDet1$dateTime)),
+           x = "", 
+           y = "mean SNR")
+   grid.arrange(p1,p2, nrow =2)
+   
+}
+plotDriftCases <- function(RawDet2,xAxisRes= '3 hours')
+      {
+   A <-  RawDet2 %>% group_by(TAG,BS) %>% 
+      arrange(TIME) %>% 
+      mutate(TimeDiff=SAMPLES_CLK-lag(SAMPLES_CLK)) %>% 
+      ungroup()%>% 
+      filter(TimeDiff>8e6/2&TimeDiff<8e6*1.5)
+   
+   A <- A %>%     pivot_wider(id_cols= c('TAG','TIME','dateTime'),names_from = BS, values_from = TimeDiff,names_prefix='Diff') %>% 
+      mutate(meanDiff=rowMeans(select(.,starts_with('Diff')),na.rm=T),N=rowSums(select(.,starts_with('Diff')),na.rm=T)/meanDiff)
+   A <- A %>% pivot_longer(
+      cols = starts_with("Diff"),
+      names_to = "BS",
+      names_prefix = "Diff",
+      values_to='sampDiff',
+      values_drop_na=T
+   ) 
+   A$samp_div <- abs(A$meanDiff-A$sampDiff)
+   A <- A %>% filter(samp_div>10 | N<2)
+   
+   D <- as.data.frame(table(A$BS,date(A$dateTime)))
+   colnames(D) <- c('BS','Date','Count')
+   D <- D %>% filter(Count>10)
+   
+   start_time <- A$dateTime[1]
+   p1 <- A %>%  filter(samp_div>10) %>%
+      ggplot(aes(dateTime, samp_div))+
+      geom_point(aes(x=dateTime,y= samp_div,group=BS, color=BS) )+
+      # geom_line(aes(x=dateTime,y= samp_div,group=BS, color=BS) ) +
+      scale_x_datetime(date_breaks = xAxisRes)+
+      theme(axis.text.x=element_text(angle=60, hjust=1),
+            legend.position=c(0.97, 0.5),
+            legend.background = element_rect(color="black",fill ="grey",#transparent",
+                                             size=0.5, linetype="solid")) +
+      labs(title= paste('Beacon detection Base station Sample clock drift '),
+           subtitle = paste('period= ',RawDet2$dateTime[1],' - ',max(RawDet2$dateTime)),
+           x = "", 
+           y = "sample clock drift")
 
-# Start_Time_Str     <- c('2021-05-17 00:00:00')
-# End_Time_Str       <- c('2021-05-19 00:01:00')
-# sample_timesPerDay <- 24
-# sample_lengthMinute<- 1
-# FullTag <- c(972006000003,972006000004,972006000006)
-# RawDet1 <- Get_ATLAS_Det_DATA (Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag )
-# plotCountDet(RawDet1,xAxisRes='1 hours')
-# plotBSperformance(RawDet1,BS2plot=c("04", "07") ,xAxisRes='1 hours')
- 
+   
+   p2 <- A %>% filter(N<2 ) %>%mutate(Tag_BS=paste(BS,"-",TAG ), ) %>% 
+      group_by(Tag_BS) %>% mutate(count_TAG_BS=n()) %>% ungroup() %>% 
+      ggplot(aes(dateTime, factor(paste(BS,"-",TAG ))))+
+      geom_point(aes(x=dateTime,y= factor(paste(BS,"-",TAG )),group=TAG, color=TAG) )+
+      # geom_line(aes(x=dateTime,y= factor(paste(BS,"-",TAG )),group=TAG, color=TAG)) +
+      geom_text(aes(x=start_time, y=Tag_BS, label = count_TAG_BS, hjust = +1.1))+
+      scale_x_datetime(date_breaks = xAxisRes)+
+      theme(axis.text.x=element_text(angle=60, hjust=1)) +
+      labs(title= paste('unsynced detections'),
+           subtitle = paste('period= ',RawDet2$dateTime[1],' - ',max(RawDet2$dateTime)),
+           x = "", 
+           y = "Base Stations- Beacons")
+   
+   grid.arrange(p1,p2, nrow = 2)
+   return(D)
+   
+}
+
+Start_Time_Str     <- c('2021-05-31 07:00:00')
+End_Time_Str       <- c('2021-05-31 09:00:00')
+sample_timesPerDay <- 24*60
+sample_lengthMinute<- 1
+FullTag <- c(972006000003,972006000004,972006000006)
+# 
+RawDet1 <- Get_ATLAS_Det_DATA (Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag )
+# 
+plotCountDet(RawDet1,xAxisRes='1 min')
+plotBSperformance(RawDet1,BS2plot=c( "03") ,xAxisRes='1 hours')
+# 
+# 
+RawDet2 <- Get_ATLAS_Det_DATA (Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag,slice = F,Fields='TAG,TIME,BS,SAMPLES_CLK' )
+plotDriftCases(RawDet2,xAxisRes='1 min')
+# 
+# FullTag2 <- (seq(from = 972006000007, to =972006001000, by = 1))
+# RawDet2 <- Get_ATLAS_Det_DATA (Start_Time_Str,End_Time_Str,sample_timesPerDay,sample_lengthMinute,FullTag2 )
+# plotTagsCount(RawDet2,xAxisRes='3 hours') # ,BS2plot=c("01","02","03","04","07")
+
+
+
+
+
