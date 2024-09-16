84af9e877dffb0301b1387bdf00071c23b4fde71
ntsur <tsur.neta@gmail.com>
2024-09-17 02:05:28 +0300

    added documentation and fixed indentation in Track_cpp.R

--- Track_cpp.R
+++ Track_cpp.R
@@ -1,16 +1,17 @@
 #http://adv-r.had.co.nz/Rcpp.html
-#install.packages('Rcpp')
-require('Rcpp')
-# knitr:::input_dir()
-print(' the file Track_cpp.R runs an essential sourceCpp commnad,
+
+print(' Attention: the file Track_cpp.R runs an essential sourceCpp commnad,
         if your current directory (getwd()) does not contain a functions/TrackConf.cpp file
         you can source it beforehand and inhibit the command form the file')
-sourceCpp('functions/TrackConf.cpp')
 
-# IdxDist(as.matrix(RawLoc1 %>% select(X,Y)),0,1)
-# pointConfidanceLevel(as.matrix(RawLoc1 %>% dplyr::select(X,Y,NBS,stdVarXY)),0,0,0)
-# TrackConfidanceVec(as.matrix(A %>% dplyr::select(X,Y,NBS,stdVarXY)),160,80)
+### Load the required packages to run this script
+require('Rcpp')
+library(dplyr)
+sourceCpp('TrackConf.cpp')
 
+# knitr:::input_dir()
+
+# Evaluate the confidence level of each localization in 'Data', using the functions in TrackConf.cpp
 TrackConfidanceLevelcpp <- function(Data,conectedVel=20,conectedDist=NA,stdlim=80,minNBSforConf2=7,
                                     minNBSforConf1=4,Nconf1forConf2=5)
 {
@@ -19,43 +20,99 @@ TrackConfidanceLevelcpp <- function(Data,conectedVel=20,conectedDist=NA,stdlim=8
   # it runs a loop on all data points per tag and gives higher confidence mark (in the range [0,1,2] )
   # to points which have large NBS or are close to confidant points
  
+  # Arguments:
+  #
+  #   'Data': the dataset, which contains the localizations for which the confidence level should be evaluated
+  #
+  #   'minNBSforConf1', 'minNBSforConf2': the minimal allowed numbers of Base Stations participated in 
+  #  	   the evaluation of the localization of the observed data point, used for the assessment of confidence level 1 and 2, respectively
+  #
+  #   'Nconf1forConf2': the minimal number of connected points with confidence level "1", which is used as a threshold to assess
+  #		   if the observed localization point should obtain a confidence level of "2".
+  #
+  #   'conectedDist': the threshold distance [m] used to determine if two analyzed localisations are 'connected'(*).
+  #
+  #   'conectedVel': the speed [m/s] used for calculation of the threshold distance, 
+  #	  	 which defines if two localization points are "connected"(*)-
+  #
+  #   'stdlim': the maximal allowed value of 'stdVARXY' value.
+  #
+  #   (*) A localization point is considered "connected" if its' distance from another confident point,
+  #  		distance= 'conectedVel' * 'numinalTimeDifference', is smaller than the threshold 'conectedDist'.
+  #   	The variable 'minimalTimeDifference' is the minimal time-difference between sampling points within the dataset. 
   
-  
+  # Check that all the data necessary for the confidence evaluation is available
+  # If not all the mentioned columns below are included in the data set, stop the function from running and output a warning
   if (!all(c('TAG','X','Y','NBS','TIME') %in% names(Data)))
   {stop("TrackConfidanceLevel needs the variables TAG,X,Y,NBS, and TIME (in miliseconds) to run ")}
   
-  if (!('val2' %in% names(Data)))
+  # Calculate the larger eigenvalue of the covariate matrix of the X and Y coordinates of the location
+  if (!('val2' %in% names(Data))) 
   {
-    if (!all(c('VARX','VARY','COVXY') %in% names(Data)))
-    {stop("TrackConfidanceLevel needs either val2 or VARX,VARY,COVXY to run")}
+    if (!all(c('VARX','VARY','COVXY') %in% names(Data))) {
+      stop("TrackConfidanceLevel needs either val2 or VARX,VARY,COVXY to run")
+    }
+    
+    # TODO: change val2 to stdVarXY
     print("TrackConfidanceLevel calculates stdVarXY as greater eigenvalue")
     Data <- Data %>% mutate(val2= sqrt(((VARX+VARY)+sqrt(VARX^2+VARY^2-2*VARX*VARY+4*COVXY^2))/2)) # greater eigenvalue
   }
   
+  # Rearrange the data rows 'Data' from the beginning-time to the end-time (ascending order)
   Data <- Data %>%  arrange(TIME)
   
-  if(!('Conf' %in% names(Data)))
-    {Data$Conf=-1}
+  # If the column 'Conf' does not exist in 'Data', add it and assign a default value of -1 to all the rows
+  if(!('Conf' %in% names(Data))) {
+    Data$Conf=-1
+  }
+  
+  # Create a new column called 'aBS', and set its' values to be the same as those in the existing 'NBS' column
   Data$aBS <- Data$NBS
   
+  # Create an empty list for the filtered data by TAG.
+  # Each element in this list will include all the data and the confidence filter of the ATLAS data from each tag.
   listoffilteredData <- list()
+  
+  # Extract the unique values from the 'TAG' column of 'Data'
   tags <- unique(Data$TAG)
+  
+  # Run on 'tags'
   for (tagInd in 1:length(unique(Data$TAG)))
   {
-  tagData <- Data %>% dplyr::filter(TAG==tags[tagInd])
-  timediffs=unique(tagData$TIME[1:min(nrow(tagData),1e4)]-lag(tagData$TIME[1:min(nrow(tagData),1e4)]))/1000
-  numinalTimeDiff <- min(round(timediffs)[which(round(timediffs)>0.5)])
-  if (is.na(conectedDist))
-      {conectedDist <- numinalTimeDiff*conectedVel}
-  tagData$Conf <-TrackConfidanceVec(as.matrix(tagData %>% dplyr::select(X,Y,aBS,stdVarXY)),
-                                    minNBSforConf2,minNBSforConf1,Nconf1forConf2,conectedDist,stdlim)
-  listoffilteredData[[tagInd]] <- tagData
+    # Filter data for the current tag (find all the data which have the current tag number)
+    tagData <- Data %>% dplyr::filter(TAG==tags[tagInd])
+    
+    # Calculate the unique time differences between consecutive time stamps in the 'tagData' data frame 
+    # and converts these differences from milliseconds to seconds
+    timediffs=unique(tagData$TIME[1:min(nrow(tagData),1e4)]-lag(tagData$TIME[1:min(nrow(tagData),1e4)]))/1000
+    
+    # Find the smallest unique time difference that is greater than 0.5 seconds
+    numinalTimeDiff <- min(round(timediffs)[which(round(timediffs)>0.5)])
+    
+    # Calculate the threshold distance, which will later be used to determine if two localizations are "connected"
+    if (is.na(conectedDist)) {
+      conectedDist <- numinalTimeDiff*conectedVel
+    }
+    
+    # Apply the function 'TrackConfidanceVec' from the file 'TrackConf.cpp'
+    tagData$Conf <-TrackConfidanceVec(as.matrix(tagData %>% dplyr::select(X,Y,aBS,stdVarXY)),
+                                      minNBSforConf2,minNBSforConf1,Nconf1forConf2,conectedDist,stdlim)
+    
+    # Store the filtered data in the previously created 'listoffilteredData'
+    listoffilteredData[[tagInd]] <- tagData
   }
+  
+  # Combine the list of data frames 'listoffilteredData' into a single data frame called 'Data'
   Data <- do.call(rbind.data.frame,listoffilteredData)
 
+  # Remove the column 'aBS' from 'Data' - WHY DID THEY CREATE THIS COLUMN AND NOT USED THE COLUMN 'NBS' IN 'TrackConfidanceVec'?
   Data <- Data %>% dplyr::select(-aBS)
+  
+  # Return 'Data'
   return(Data)
 }
+
+
 # A <- RawLoc0 %>% filter(TAG>0)
 # B <- TrackConfidanceLevel(A,20,80)
 # C <- TrackConfidanceLevelcpp(A,20,80)
