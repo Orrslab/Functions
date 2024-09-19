a35e11141be0cdc12f8656d2ff87c68712a6a6e8
ntsur <tsur.neta@gmail.com>
2024-09-19 16:35:17 +0300

    created a function to convert unix timestamp to a human readable ATLAS time format

--- time_conversions.R
+++ time_conversions.R
@@ -23,3 +23,26 @@ time_str_to_utc_timestamp <- function(timestring)
   return(utc_timestamp)
   
 }
+
+
+#' Convert Unix Timestamp to a Human-readable Date in ATLAS Format
+#'
+#' This function converts Unix timestamps (in milliseconds) to a human-readable date format 
+#' according to the ATLAS system specifications. 
+#' It handles both single timestamp values and vectors of Unix Timestamps.
+#'
+#' @param unix_timestamp A numeric vector or single numeric value representing Unix timestamps in milliseconds.
+#'
+#' @return A character vector of formatted date and time strings in the ATLAS format. 
+#' If the input is a single value, the return will be a single string.
+#' 
+unix_timestamp_to_human_date <- function(unix_timestamp) {
+  # Convert time from milliseconds to seconds
+  timestamp_seconds <- unix_timestamp / 1000 
+  # Convert to POSIXct object
+  converted_atlas_time <- as.POSIXct(timestamp_seconds, origin = "1970-01-01", tz = atlas_time_zone)
+  # Format the POSIXct object to the desired format
+  atlas_time_formatted <- format(converted_atlas_time, atlas_time_format)
+  return(atlas_time_formatted)
+}
+
