2fd83657beb401d5d4a9dfdbec32a0e574d62f87
ntsur <tsur.neta@gmail.com>
2024-09-22 21:49:51 +0300

    turned the code into a pipeline, which either retrieves new data from the ATLAS server and saves them into sqlite, or loads a previously- saved sqlite file.

--- Scripts/get_ATLAS_data.R
+++ Scripts/get_ATLAS_data.R
@@ -2,26 +2,35 @@
 source(file.path(getwd(), "Scripts", "config.R"))
 
 
-# Download ATLAS data directly from the server
-# This requires a VPN connection to the TAU server
-source(paste0(path_to_atlas_data_analysis_repo, "Data_from_ATLAS_server.R"))
+if (load_data_from_sqlite_file) {
+  
+  print("Load ATLAS data from an SQLite file")
+  
+} else {
+  
+  # Download ATLAS data directly from the server
+  # This requires a VPN connection to the TAU server
+  source(paste0(path_to_atlas_data_analysis_repo, "Data_from_ATLAS_server.R"))
+  
+  AllData <- Data_from_ATLAS_server(Start_Time_Str,
+                                    End_Time_Str,
+                                    tag_numbers, 
+                                    includeDet=FALSE)
+  
+  RawLoc0 <- AllData$LOC
+  RawDet0 <- AllData$DET
+  
+  if (save_data_to_sqlite_file) {
+    
+    source(paste0(path_to_atlas_data_analysis_repo, "save_ATLAS_data_to_sqlite.R"))
+    
+    save_ATLAS_data_to_sqlite(localizations_data = RawLoc0, 
+                              tag_number = tag_numbers, 
+                              start_time = Start_Time_Str,
+                              end_time = End_Time_Str)
+  }
+}
 
-AllData <- Data_from_ATLAS_server(Start_Time_Str,
-                                  End_Time_Str,
-                                  tag_numbers, 
-                                  includeDet=FALSE)
-
-RawLoc0 <- AllData$LOC
-RawDet0 <- AllData$DET
-
-source(paste0(path_to_atlas_data_analysis_repo, "save_ATLAS_data_to_sqlite.R"))
-
-print(1)
-
-save_ATLAS_data_to_sqlite(localizations_data = RawLoc0, 
-                          tag_number = tag_numbers, 
-                          start_time = Start_Time_Str,
-                          end_time = End_Time_Str)
 
   
 
