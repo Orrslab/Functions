b13a04c2dde4561e4f93df1f641e69a34fd040a2
ntsur <tsur.neta@gmail.com>
2024-09-23 00:13:47 +0300

    moved code into separate function 'retrieve_and_store_atlas_data'.

--- Scripts/get_ATLAS_data.R
+++ Scripts/get_ATLAS_data.R
@@ -15,25 +15,9 @@ if (load_data_from_sqlite_file) {
   
   # Download ATLAS data directly from the server
   # This requires a VPN connection to the TAU server
-  source(paste0(path_to_atlas_data_analysis_repo, "Data_from_ATLAS_server.R"))
-  
-  AllData <- Data_from_ATLAS_server(Start_Time_Str,
-                                    End_Time_Str,
-                                    tag_numbers, 
-                                    includeDet=FALSE)
-  
-  RawLoc0 <- AllData$LOC
-  RawDet0 <- AllData$DET
-  
-  if (save_data_to_sqlite_file) {
-    
-    source(paste0(path_to_atlas_data_analysis_repo, "save_ATLAS_data_to_sqlite.R"))
-    
-    save_ATLAS_data_to_sqlite(localizations_data = RawLoc0, 
-                              tag_number = tag_numbers, 
-                              start_time = Start_Time_Str,
-                              end_time = End_Time_Str)
-  }
+
+  source(paste0(path_to_atlas_data_analysis_repo, "retrieve_and_store_atlas_data.R"))
+  RawLoc0 <- retrieve_and_store_atlas_data(data_requests)
 }
 
 
