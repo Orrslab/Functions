f1be5a6ea327831d1ea26d914351e0aa6b402126
ntsur <tsur.neta@gmail.com>
2024-09-22 20:14:49 +0300

    added a function that creates the sqlite file name and path.

--- save_ATLAS_data_to_sqlite.R
+++ save_ATLAS_data_to_sqlite.R
@@ -4,33 +4,42 @@ sapply(required_packages, library, character.only = TRUE)
 
 source(file.path(getwd(), "Scripts", "config.R"))
 
-
-#' Save ATLAS Data to SQLite Database
+#' Create the SQLite file path from tag numbers and date range
 #'
-#' This function saves localization and detection data to an SQLite database.
-#' The database file is named based on the provided tag number, start time, 
-#' and end time. Spaces in the time strings are replaced with underscores.
+#' This function generates a file path for the SQLite database based on the 
+#' provided tag numbers and the date range of the data. If there is a single 
+#' tag, the last four digits of the tag number will be used in the filename. 
+#' If there are multiple tags, "Multiple_tags" will be used instead. 
+#' The function also formats the date and time by replacing spaces with 
+#' underscores and colons with hyphens.
 #'
-#' @param localizations_data A data frame containing localization data. 
-#'                           Default is NULL, meaning the data will not be saved if not provided.
-#' @param detections_data A data frame containing detection data. 
-#'                        Default is NULL, meaning the data will not be saved if not provided.
-#' @param tag_number A unique identifier for the tag associated with the data.
+#' @param tag_numbers A unique identifier (or identifiers) for the tag(s) 
+#'                    associated with the data. If a single tag number is 
+#'                    provided, the filename will include the last four digits 
+#'                    of the tag number. If multiple tags are provided, the 
+#'                    filename will include "Multiple_tags".
 #' @param start_time A character string representing the start time in 
-#'                   the format 'YYYY-MM-DD HH:MM:SS'.
+#'                   the format 'YYYY-MM-DD HH:MM:SS'. The function will 
+#'                   format this by replacing spaces with underscores and 
+#'                   colons with hyphens.
 #' @param end_time A character string representing the end time in 
-#'                 the format 'YYYY-MM-DD HH:MM:SS'.
+#'                 the format 'YYYY-MM-DD HH:MM:SS'. The function will 
+#'                 format this by replacing spaces with underscores and 
+#'                 colons with hyphens.
 #'
-#' @return This function does not return any value. It saves the provided 
-#'         data frames to the specified SQLite database file.
-#'
-#' @details The function connects to an SQLite database and writes the 
-#'          localization and detection data to separate tables. If either 
-#'          data frame is not provided, the corresponding table will not be created.
-#'          The database connection is closed after the operation is complete.
-#'          
-save_ATLAS_data_to_sqlite <- function(localizations_data=NULL, detections_data=NULL, tag_number, start_time, end_time)
-{
+#' @return A character string representing the file path for the SQLite 
+#'         database, including the formatted tag numbers and date range.
+#'  
+create_sqlite_filepath <- function(tag_numbers, start_time, end_time) {
+  
+  if (length(tag_numbers) == 1) {
+    # Take the last four digits of the tag number
+    tag_number_str <- substr(as.character(tag_numbers), nchar(tag_numbers)-3, nchar(tag_numbers))
+    tag_number_str <- paste0("Tag_", tag_number_str)
+  } else {
+    tag_number_str = "Multiple_tags"
+  }
+  
   # Replace spaces with underscores
   start_time_replace_spaces <- gsub(" ", "_", start_time)
   end_time_replace_spaces <- gsub(" ", "_", end_time)
@@ -40,12 +49,47 @@ save_ATLAS_data_to_sqlite <- function(localizations_data=NULL, detections_data=N
   end_time_replace_colons <- gsub(":", "-", end_time_replace_spaces)
   
   # Create the .sqlite file name
-  filename <- paste("db_", tag_number, "_", 
+  filename <- paste(tag_number_str, "_from_", 
                     start_time_replace_colons, "_to_", 
                     end_time_replace_colons, ".sqlite", sep = "")
   filepath = paste0(path_to_sqlite_files, filename)
-  print(filepath)
-  
+  return(filepath)
+}
+
+
+#' Save ATLAS Data to SQLite Database
+#'
+#' This function saves localization and detection data to an SQLite database. 
+#' The database file is named based on the provided tag number(s), start time, 
+#' and end time. The function calls `create_sqlite_filepath` to generate the 
+#' appropriate filename and file path.
+#'
+#' @param localizations_data A data frame containing localization data. 
+#'                           Default is NULL, meaning the data will not be saved if not provided.
+#' @param detections_data A data frame containing detection data. 
+#'                        Default is NULL, meaning the data will not be saved if not provided.
+#' @param tag_numbers A unique identifier (or identifiers) for the tag(s) 
+#'                    associated with the data. Used to generate the SQLite 
+#'                    file name.
+#' @param start_time A character string representing the start time in the 
+#'                   format 'YYYY-MM-DD HH:MM:SS'. Used to generate the 
+#'                   SQLite file name.
+#' @param end_time A character string representing the end time in the 
+#'                 format 'YYYY-MM-DD HH:MM:SS'. Used to generate the 
+#'                 SQLite file name.
+#'
+#' @return This function does not return any value. It saves the provided 
+#'         data frames to the specified SQLite database file.
+#'
+#' @details The function connects to an SQLite database and writes the 
+#'          localization and detection data to separate tables. If either 
+#'          data frame is not provided, the corresponding table will not be created.
+#'          The database connection is closed after the operation is complete.
+#'          
+save_ATLAS_data_to_sqlite <- function(localizations_data=NULL, detections_data=NULL, tag_numbers, start_time, end_time)
+{
+  filepath <- create_sqlite_filepath(tag_numbers, start_time, end_time)
+
   # Connect to the SQLite database
   conn <- dbConnect(RSQLite::SQLite(), dbname = filepath)
   
@@ -63,5 +107,4 @@ save_ATLAS_data_to_sqlite <- function(localizations_data=NULL, detections_data=N
   
   # Close the database connection
   dbDisconnect(conn)
-  
 }
