7dd128f4432937bb0c4bf2a1d96bef3a7d0fed9c
ntsur <tsur.neta@gmail.com>
2024-09-23 02:49:54 +0300

    added functions to load atlas data from multiple sqlite files.

---
+++ create_list_of_sqlite_filepaths.R
@@ -0,0 +1,33 @@
+source(file.path(getwd(), "Scripts", "config.R"))
+source(paste0(path_to_atlas_data_analysis_repo, "create_sqlite_filepath.R"))
+
+#' Create a list of SQLite file paths based on data requests.
+#'
+#' This function generates a list of SQLite file paths by extracting tag 
+#' numbers, start times, and end times from the provided data requests.
+#'
+#' @param data_requests A list of lists (or a dataframe) where each element 
+#'                      contains:
+#'                      - tag: A character string representing the tag number.
+#'                      - start_time: A character string representing the start time.
+#'                      - end_time: A character string representing the end time.
+#'
+#' @return A list of character strings representing the paths to the SQLite files.
+#'
+create_list_of_sqlite_filepaths <- function(data_requests) {
+  
+  filepaths <- list()
+  
+  for (request in data_requests) {
+    
+    tag_numbers = request$tag
+    start_time = request$start_time
+    end_time = request$end_time
+    
+    filepaths[[length(filepaths) + 1]] <- create_sqlite_filepath(tag_numbers,
+                                                                 start_time,
+                                                                 end_time)
+    
+  }
+  return(filepaths)
+}
