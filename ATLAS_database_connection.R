ece67472447bf3fb009178d97399f92acbfcf6bd
ntsur <tsur.neta@gmail.com>
2024-09-23 00:50:26 +0300

    moved connection to the atlas database into ATLAS_database_connection

--- ATLAS_database_connection.R
+++ ATLAS_database_connection.R
@@ -1,3 +1,8 @@
+
+# Load the required packages
+required_packages <- c("DBI", "RMySQL")
+sapply(required_packages, library, character.only = TRUE)
+
 #' Connect to an ATLAS Database
 #'
 #' This function establishes a connection to any ATLAS database, given the required credentials.
@@ -10,7 +15,22 @@
 #'
 #' @return A database connection object (\code{dbc}) if the connection is successful.
 
-connect_to_atlas_db <- function(db_username, db_pass, db_host_ip, db_port_number, db_name) {
+connect_to_atlas_db <- function(SYS=system_name_harod) {
+  
+  # Get the credentials of the ATLAS database you need to connect to
+  
+  if (SYS==system_name_harod) {
+    # Get the Harod database credentials from the configuration file
+    db_username = db_username_harod         # username
+    db_pass = db_pass_harod                 # password
+    db_host_ip = db_host_ip_harod           # host ip address
+    db_port_number = db_port_number_harod   # port Number
+    db_name = db_name_harod                 # name of data base
+    
+  } else {
+    stop(glue("Warning: system {SYS} is not defined"))
+  }
+  
   # Establish a connection to the specified ATLAS database
   dbc <- dbConnect(RMySQL::MySQL(),
                    user = db_username,
--- retrieve_and_store_atlas_data.R
+++ retrieve_and_store_atlas_data.R
@@ -2,37 +2,16 @@
 # Load the functions that connect to the ATLAS database
 source(paste0(path_to_atlas_data_analysis_repo, "ATLAS_database_connection.R"))
 
-retrieve_and_store_atlas_data <- function(data_requests, SYS=system_name_harod) {
-  
-  # Get the credentials of the ATLAS database you need to connect to
-  
-  if (SYS==system_name_harod) {
-    # Get the Harod database credentials from the configuration file
-    db_username = db_username_harod         # username
-    db_pass = db_pass_harod                 # password
-    db_host_ip = db_host_ip_harod           # host ip address
-    db_port_number = db_port_number_harod   # port Number
-    db_name = db_name_harod                 # name of data base
-    
-  } else {
-    stop(glue("Warning: system {SYS} is not defined"))
-  }
+retrieve_and_store_atlas_data <- function(data_requests) {
   
   #connect to the database
-  db_conn <- connect_to_atlas_db(db_username, 
-                                 db_pass, 
-                                 db_host_ip, 
-                                 db_port_number, 
-                                 db_name)
+  db_conn <- connect_to_atlas_db()
   
   for (request in data_requests) {
     
     tag_numbers <- request$tag
     start_time <- request$start_time
     end_time <- request$end_time
-    
-    print(start_time)
-    print(end_time)
     
     source(paste0(path_to_atlas_data_analysis_repo, "Data_from_ATLAS_server.R"))
     AllData <- Data_from_ATLAS_server(start_time,
