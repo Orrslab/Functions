384633c8609534dfb8004949687f5cb69261aff0
ntsur <tsur.neta@gmail.com>
2024-10-07 15:25:40 +0300

    added a data_requests file with explanation and examples.

--- ATLAS_run_scripts/ATLAS_data_requests.R
+++ ATLAS_run_scripts/ATLAS_data_requests.R
@@ -1,41 +1,14 @@
+# This file is for setting the tag numbers and date ranges 
+# for the data retrieval from the ATLAS server, 
+# or to load data from previously-saved SQLite files.
 
+# Each item in the 'data_requests' list specifies one batch of tag numbers, start time, and end time.
+# The user can modify the number of elements in 'data_requests', 
+# Also, the user can create different files for different projects.
+# If doing so, make sure to specify in the config file, from where to read the relevant data_requests file.
+
+# An example for possible inputs
 data_requests <- list(
-  list(tag = 972006000426, start_time = '2022-04-10 00:00:01', end_time = '2022-04-10 23:59:00'),
-  list(tag = 972006000442, start_time = '2022-04-10 00:00:01', end_time = '2022-04-10 23:59:00'),
-  list(tag = 972006000584, start_time = '2022-03-19 00:00:01', end_time = '2022-03-19 23:59:00')
+  list(tag = 972006000836, start_time = '2023-12-24 00:00:01', end_time = '2023-12-25 00:00:01'),
+  list(tag = c(972006000837, 972006000841, 972006000847), start_time = '2024-09-05 08:00:00', end_time = '2024-09-05 09:00:00')
 )
-
-# data_requests <- list(
-#   list(tag = 972006000442, start_time = '2022-04-01 00:00:01', end_time = '2022-04-01 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-02 00:00:01', end_time = '2022-04-02 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-03 00:00:01', end_time = '2022-04-03 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-04 00:00:01', end_time = '2022-04-04 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-05 00:00:01', end_time = '2022-04-05 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-06 00:00:01', end_time = '2022-04-06 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-07 00:00:01', end_time = '2022-04-07 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-08 00:00:01', end_time = '2022-04-08 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-09 00:00:01', end_time = '2022-04-09 23:59:00'),
-#   list(tag = 972006000442, start_time = '2022-04-10 00:00:01', end_time = '2022-04-10 23:59:00')
-# )
-
-# data_requests <- list(
-#   list(tag = 972006000584, start_time = '2022-03-20 00:00:01', end_time = '2022-03-20 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-21 00:00:01', end_time = '2022-03-21 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-22 00:00:01', end_time = '2022-03-22 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-23 00:00:01', end_time = '2022-03-23 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-24 00:00:01', end_time = '2022-03-24 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-25 00:00:01', end_time = '2022-03-25 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-26 00:00:01', end_time = '2022-03-26 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-27 00:00:01', end_time = '2022-03-27 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-28 00:00:01', end_time = '2022-03-28 23:59:00'),
-#   list(tag = 972006000584, start_time = '2022-03-29 00:00:01', end_time = '2022-03-29 23:59:00')
-# )
-
-# data_requests <- list(
-#   list(tag = c(972006000837, 972006000841, 972006000847), start_time = '2024-09-05 08:00:00', end_time = '2024-09-05 09:00:00'),
-#   list(tag = 972006000836, start_time = '2023-12-24 00:00:01', end_time = '2023-12-25 00:00:01')
-# )
-# 
-# data_requests <- list(
-#   list(tag = 972006000836, start_time = '2023-12-24 00:00:01', end_time = '2023-12-25 00:00:01')
-# )
