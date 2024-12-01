24705d06e281fa5c925fe4040faa8862287ceb61
ntsur <tsur.neta@gmail.com>
2024-12-01 14:09:18 +0200

    modified markers colors to purple and yellow.

--- ATLAS_maps/interactive_maps.R
+++ ATLAS_maps/interactive_maps.R
@@ -158,7 +158,7 @@ atl_mapleaf2 <- function(dd1,dd2,MapProvider='Esri.WorldImagery',legendLabels=c(
     addProviderTiles(MapProvider) %>%
     
     # Add circles at the locations of the first dataset 'dd1'
-    addCircles(data = llpd1_sf, weight = 1, fillOpacity = 1, color = col[4], group = legendLabels[1],
+    addCircles(data = llpd1_sf, weight = 1, fillOpacity = 1, color = "yellow", group = legendLabels[1],
                popup = ~htmlEscape(paste0("1:time=", as.character(llpd1_sf$dateTimeFormatted),
                                           ", TIME=", as.character(llpd1_sf$TIME),
                                           ", Z=", as.character(llpd1_sf$Z),
@@ -177,10 +177,10 @@ atl_mapleaf2 <- function(dd1,dd2,MapProvider='Esri.WorldImagery',legendLabels=c(
                                           ", TAG=", llpd1_sf$TAG))) %>%
 
     # Add lines that connect the point locations included in 'dd1'
-    addPolylines(data = llpd1_lines, weight = 1, opacity = 1, color = col[4], group = legendLabels[1]) %>%
+    addPolylines(data = llpd1_lines, weight = 1, opacity = 1, color = "yellow", group = legendLabels[1]) %>%
     
     # Add circles at the locations of the first dataset 'dd2'
-    addCircles(data = llpd2_sf, weight = 1, fillOpacity = 1, color = col[3], group = legendLabels[2],
+    addCircles(data = llpd2_sf, weight = 1, fillOpacity = 1, color = "purple", group = legendLabels[2],
                popup = ~htmlEscape(paste0("2:time=", as.character(llpd2_sf$dateTimeFormatted),
                                           ", TIME=", as.character(llpd2_sf$TIME),
                                           ", Z=", as.character(llpd2_sf$Z),
@@ -199,7 +199,7 @@ atl_mapleaf2 <- function(dd1,dd2,MapProvider='Esri.WorldImagery',legendLabels=c(
                                           ", TAG=", llpd2_sf$TAG))) %>%
     
     # Add lines that connect the point locations included in 'dd2'
-    addPolylines(data = llpd2_lines, weight = 1, opacity = 1, color = col[3], group = legendLabels[2]) %>%
+    # addPolylines(data = llpd2_lines, weight = 1, opacity = 1, color = "purple", group = legendLabels[2]) %>%
     
     # Add a scale bar to the map
     addScaleBar(position = c("bottomleft"), options = scaleBarOptions(imperial = FALSE, maxWidth = 200)) %>%
