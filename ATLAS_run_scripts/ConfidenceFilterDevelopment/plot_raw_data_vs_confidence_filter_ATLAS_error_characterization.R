ad444ae4fceba5bdd71ad6f3e82a99018f3bcd9b
ntsur <tsur.neta@gmail.com>
2024-12-03 14:39:16 +0200

    added a map of a single data set

--- ATLAS_run_scripts/ConfidenceFilterDevelopment/plot_raw_data_vs_confidence_filter_ATLAS_error_characterization.R
+++ ATLAS_run_scripts/ConfidenceFilterDevelopment/plot_raw_data_vs_confidence_filter_ATLAS_error_characterization.R
@@ -3,17 +3,21 @@
 source(file.path(getwd(), "config.R"))
 
 source(paste0(path_to_scripts,"ATLAS_data_analysis_main.R"))
-source(paste0(path_to_atlas_data_analysis_repo,"atl_plots.R"))
+source(paste0(path_to_atlas_maps,"interactive_maps.R"))
 
 library(leaflet)
 library(sf)
 library(RColorBrewer)
 library(htmltools)
 
-# Generate a map of the raw data vs filtered data with confidence = 2
-map <- atl_mapleaf2(dd1=raw_data_with_confidence_levels,
-             dd2=data__with_confidence_2,
-             MapProvider='Esri.WorldImagery',
-             legendLabels=c("Raw Data", "Data with Confidence = 2")) 
-  
+# Generate a map of a single data set
+map <- atl_mapleaf(dd=raw_location_data)
 print(map)
+
+# # Generate a map of the raw data vs filtered data with confidence = 2
+# map <- atl_mapleaf2(dd1=raw_data_with_confidence_levels,
+#              dd2=data__with_confidence_2,
+#              MapProvider='Esri.WorldImagery',
+#              legendLabels=c("Raw Data", "Data with Confidence = 2")) 
+#   
+# print(map)
