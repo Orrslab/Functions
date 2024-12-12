de4fb34c1baec3cde2ba89d7008b6c103d7aa211
ntsur <tsur.neta@gmail.com>
2024-12-12 15:41:45 +0200

    added a function that prepares the raw data before it is uploaded on the visual filter app.

--- prepare_raw_atlas_data_for_visual_filter.R
+++ prepare_raw_atlas_data_for_visual_filter.R
@@ -1,18 +1,25 @@
 # Get ATLAS data and prepare it for using the visual filter
 
-prepare_atlas_data_for_visual_filter <- function(data_requests, folder_paths_to_retrieve_sqlite_files=NULL, folder_path_to_save_sqlite=NULL)
+prepare_raw_atlas_data_for_visual_filter <- function(data_requests,
+                                                     retrieve_data_from_server,
+                                                     save_data_to_sqlite_file,
+                                                     raw_data_folder_path)
 {
-  # Get the required paths from the config file config.R
-  source(file.path(getwd(), "config.R"))
   
   # # Install the required R packages- in not yet installed
   # source(paste0(path_to_scripts,"install_required_R_packages.R"))
   
+  # Generate the file names from the tag numbers and dates
+  source(paste0(path_to_atlas_data_analysis_repo, "create_list_of_sqlite_filepaths.R"))
+  fullpaths_to_sqlite_files <- create_list_of_sqlite_filepaths(data_requests, 
+                                                               folder_path_to_sqlite_files = raw_data_folder_path)
+  
   # Get the ATLAS data- either from the server, or from an SQLite file
   source(paste0(path_to_atlas_data_analysis_repo,"get_ATLAS_data.R"))
-  raw_location_data = get_ATLAS_data(data_requests, 
-                                     folder_paths_to_retrieve_sqlite_files,
-                                     folder_path_to_save_sqlite)
+  raw_location_data = get_ATLAS_data(data_requests = data_requests, 
+                                     retrieve_data_from_server = retrieve_data_from_server,
+                                     save_data_to_sqlite_file = save_data_to_sqlite_file,
+                                     full_paths_to_sqlite_files = fullpaths_to_sqlite_files)
   
   # Assign day numbers to the data
   source(paste0(path_to_atlas_data_analysis_repo, "time_conversions.R"))
