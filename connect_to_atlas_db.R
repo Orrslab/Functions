3c8257ab480ae410d951d23aa3e760b6a4373cd3
ntsur <tsur.neta@gmail.com>
2025-01-03 00:59:04 +0200

    changed function name

--- connect_to_atlas_db.R
+++ connect_to_atlas_db.R
@@ -15,19 +15,16 @@ sapply(required_packages, library, character.only = TRUE)
 #'
 #' @return A database connection object (\code{dbc}) if the connection is successful.
 
-connect_to_atlas_db <- function(SYS=system_name_harod) {
+connect_to_atlas_db <- function(SYS=system_name_harod, 
+                                db_username,
+                                db_pass,
+                                db_host_ip,
+                                db_port_number,
+                                db_name) {
   
   # Get the credentials of the ATLAS database you need to connect to
   
-  if (SYS==system_name_harod) {
-    # Get the Harod database credentials from the configuration file
-    db_username = db_username_harod         # username
-    db_pass = db_pass_harod                 # password
-    db_host_ip = db_host_ip_harod           # host ip address
-    db_port_number = db_port_number_harod   # port Number
-    db_name = db_name_harod                 # name of data base
-    
-  } else {
+  if (SYS!=system_name_harod) {
     stop(glue("Warning: system {SYS} is not defined"))
   }
   
