67f1f805181ef82bec92c7ff5a2af27cad25bac4
ntsur <tsur.neta@gmail.com>
2025-01-09 01:34:43 +0200

    1) is no sqlite file of the raw data exists, retrieve it from the atlas server. Else, load from sqlite. 2) changed folder name from 'path to function repo' to 'path to visual filter folder'.

--- prepare_raw_atlas_data_for_visual_filter.R
+++ prepare_raw_atlas_data_for_visual_filter.R
@@ -3,8 +3,6 @@
 prepare_raw_atlas_data_for_visual_filter <- function(tag_number,
                                                      start_time,
                                                      end_time,
-                                                     retrieve_data_from_server,
-                                                     save_data_to_sqlite_file,
                                                      raw_data_folder_path)
 {
   
@@ -18,7 +16,7 @@ prepare_raw_atlas_data_for_visual_filter <- function(tag_number,
   )
   
   # Generate the file names from the tag numbers and dates
-  source(paste0(path_to_atlas_data_analysis_repo, "create_sqlite_filepath.R"))
+  source(paste0(path_to_visual_filter_folder, "create_sqlite_filepath.R"))
   fullpath_to_sqlite_file <- create_sqlite_filepath(tag_numbers = tag_number,
                                                     start_time = start_time,
                                                     end_time = end_time,
@@ -33,28 +31,35 @@ prepare_raw_atlas_data_for_visual_filter <- function(tag_number,
     fullpath_to_sqlite_file <- paste0(substr(fullpath_to_sqlite_file, 1, pos - 1), "_raw", substr(fullpath_to_sqlite_file, pos, nchar(fullpath_to_sqlite_file)))
   }
   
+  # Check if the sqlite file already exists
+  if (file.exists(fullpath_to_sqlite_file)) {
+    retrieve_data_from_server = FALSE
+    print("Loading data from sqlite")
+  } else {
+    retrieve_data_from_server = TRUE
+    save_data_to_sqlite_file = TRUE
+    print("Retrieving data from the ATLAS server")
+  }
   
   # Get the ATLAS data- either from the server, or from an SQLite file
-  source(paste0(path_to_atlas_data_analysis_repo,"get_ATLAS_data.R"))
+  source(paste0(path_to_visual_filter_folder,"get_ATLAS_data.R"))
   raw_location_data = get_ATLAS_data(data_requests = data_request, 
                                      retrieve_data_from_server = retrieve_data_from_server,
                                      save_data_to_sqlite_file = save_data_to_sqlite_file,
                                      full_paths_to_sqlite_files = fullpath_to_sqlite_file)
   
   # Assign day numbers to the data
-  source(paste0(path_to_atlas_data_analysis_repo, "time_conversions.R"))
+  source(paste0(path_to_visual_filter_folder, "time_conversions.R"))
   # convert the time column to the POSIXct format- required for using AssignDayNumber.R
   raw_location_data$dateTime <- convert_to_POSIXct(raw_location_data$TIME)
-  source(paste0(path_to_atlas_data_analysis_repo, "AssignDayNumber.R"))
-  raw_location_data <- AssignDayNumber(data=raw_location_data, TimeColName = "dateTime")
-  
-  if (save_data_to_csv_file) {
-    # Save the raw data as CSV
-    write.csv(raw_location_data, paste0(path_to_csv_files, "BO.csv"), row.names = FALSE)
-  }
+  source(paste0(path_to_visual_filter_folder, "AssignDayNumber.R"))
+  raw_location_data <- AssignDayNumber(data=raw_location_data,
+                                       DayStartTime = day_start_time,
+                                       DayEndTime = day_end_time,
+                                       TimeColName = "dateTime")
   
   return(raw_location_data)
 }
 
 # Activate the shiny Visual Filter
-# source(paste0(path_to_atlas_data_analysis_repo, "visual_filter_shiny_app.R"))
+# source(paste0(path_to_visual_filter_folder, "visual_filter_shiny_app.R"))
