7835a2874d98d0c1a07d028f46b109a8e684e4dc
ntsur <tsur.neta@gmail.com>
2025-01-13 15:39:54 +0200

    added documentation: update_and_plot_filtered_data.R

--- upload_and_plot_filtered_data.R
+++ upload_and_plot_filtered_data.R
@@ -1,7 +1,30 @@
+#' Load, Clean, and Visualize Filtered ATLAS Data
+#'
+#' This script loads movement data from an SQLite file, removes missing values, 
+#' separates valid points from outliers, and visualizes them using a Leaflet map.
+#'
+#' @note Ensure that `load_atlas_data_from_sqlite.R` and `interactive_map_two_atlas_datasets.R` 
+#' are in the working directory before running this script.
+#'
+#' @import leaflet
+#'
+#' @example 
+#' \dontrun{
+#' # Set file name and path
+#' file_name <- "BO_0556_from_2021-07-04_17-00-03_to_2021-07-04_23-59-58_filtered.sqlite"
+#' file_path <- "C:/Users/netat/Documents/Movement_Ecology/Confidence_Filter/human_tagging_database/tagging_database/Filtered_data/"
+#'
+#' # Run the script to generate an interactive map
+#' source("this_script.R")
+#' }
 
+# USER INPUT REQUIRED
 # Set the file name and path
 file_name <- "BO_0556_from_2021-07-04_17-00-03_to_2021-07-04_23-59-58_filtered.sqlite"
 file_path <- "C:/Users/netat/Documents/Movement_Ecology/Confidence_Filter/human_tagging_database/tagging_database/Filtered_data/"
+# END OF USER INPUT
+
+# Generate the full path
 full_path <- paste0(file_path, file_name)
 
 # Load the filtered data from sqlite
