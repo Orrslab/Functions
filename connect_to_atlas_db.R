e9c1ea4c44dc966c7ca11b406384f3712f566718
ntsur <tsur.neta@gmail.com>
2025-01-13 18:43:29 +0200

    updated documentation: connect_to_atlas_db.R

--- connect_to_atlas_db.R
+++ connect_to_atlas_db.R
@@ -1,21 +1,39 @@
 
-# Load the required packages
-required_packages <- c("DBI", "RMySQL")
-sapply(required_packages, library, character.only = TRUE)
-
 #' Connect to an ATLAS Database
 #'
-#' This function establishes a connection to any ATLAS database, given the required credentials.
+#' This function establishes a connection to an ATLAS database using the provided credentials.
 #'
-#' @param db_username A character string representing the database username.
-#' @param db_pass A character string representing the database password.
-#' @param db_host_ip A character string representing the database host IP.
-#' @param db_port_number An integer representing the database port number.
-#' @param db_name A character string representing the name of the database.
+#' @param atlas_db_credentials A named list containing the database connection details. 
+#'   The list must include the following elements:
+#'   \itemize{
+#'     \item{\code{system_name}} {A character string indicating the system name (e.g., "Harod").}
+#'     \item{\code{db_username}} {A character string representing the database username.}
+#'     \item{\code{db_pass}} {A character string representing the database password.}
+#'     \item{\code{db_host_ip}} {A character string representing the database host IP.}
+#'     \item{\code{db_port_number}} {An integer representing the database port number.}
+#'     \item{\code{db_name}} {A character string representing the name of the database.}
+#'   }
 #'
-#' @return A database connection object (\code{dbc}) if the connection is successful.
-
+#' @return A database connection object (\code{DBI::DBIConnection}) if the connection is successful.
+#'
+#' @details 
+#' This function checks whether the specified system is recognized (e.g., "Harod"). 
+#' If the system is not recognized, a warning is issued. 
+#' It then attempts to establish a connection to the ATLAS database using `RMySQL::dbConnect()`. 
+#' 
+#' **Assumptions:**
+#' - The database must be accessible via the provided host, port, and credentials.
+#' - The user must have the necessary permissions to connect to the database.
+#' - The required packages (`DBI` and `RMySQL`) must be installed and loaded.
+#'
+#' @import DBI
+#' @import RMySQL
+#' @export
 connect_to_atlas_db <- function(atlas_db_credentials) {
+  
+  # Load the required packages
+  required_packages <- c("DBI", "RMySQL")
+  invisible(lapply(required_packages, library, character.only = TRUE))
   
   # Get the credentials of the ATLAS database you need to connect to
   
