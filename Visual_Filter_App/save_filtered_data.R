c8f817a9a1e724bd15d6bc88495886a7b5f8b393
ntsur <tsur.neta@gmail.com>
2025-02-13 16:48:40 +0200

    moved the helper functions out of the app's main script for a cleaner code.

---
+++ Visual_Filter_App/save_filtered_data.R
@@ -0,0 +1,31 @@
+# Helper function to save the annotated data
+save_filtered_data <- function(tag_number, start_time, end_time, 
+                               filtered_data_path, segment_data,
+                               save_as_csv = FALSE) {
+  
+  # Create the file name and full path to save the filtered data
+  full_path_filtered_data <- create_sqlite_filepath(animal_name_code,
+                                                    tag_number, 
+                                                    start_time, 
+                                                    end_time, 
+                                                    filtered_data_path)
+  
+  # Add _filtered to the file name
+  # Find the position of the last dot (before the extension)
+  pos <- regexpr("\\.sqlite$", full_path_filtered_data)
+  
+  # If a dot followed by 'sqlite' is found, insert '_raw' before it
+  if (pos > 0) {
+    full_path_filtered_data <- paste0(substr(full_path_filtered_data, 1, pos - 1), "_annotated", substr(full_path_filtered_data, pos, nchar(full_path_filtered_data)))
+  }
+  
+  # Save the current data segment as sqlite
+  source(paste0(getwd(), "/save_ATLAS_data_to_sqlite.R"))
+  save_ATLAS_data_to_sqlite(localizations_data = segment_data,
+                            fullpath = full_path_filtered_data)
+  
+  if (save_as_csv) {
+    full_path_filtered_data_csv <- sub("\\.sqlite$", ".csv", full_path_filtered_data)
+    write.csv(segment_data, full_path_filtered_data_csv, row.names = FALSE)
+  }
+}
