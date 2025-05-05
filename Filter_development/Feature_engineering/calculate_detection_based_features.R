64bdb080a21ec0f3b23d2beed693ddd7dc12dd3c
ntsur <tsur.neta@gmail.com>
2025-05-06 01:08:52 +0300

    calculate the distance of each location point from the closest base station and the number of this base station.

--- Filter_development/Feature_engineering/calculate_detection_based_features.R
+++ Filter_development/Feature_engineering/calculate_detection_based_features.R
@@ -1,13 +1,20 @@
 source(file.path(getwd(), "match_detections_to_localizations.R"))
 source(file.path(getwd(), "Filter_development/Feature_engineering/calculate_SNR_features.R"))
+source(file.path(getwd(), "Filter_development/Feature_engineering/calculate_distance_to_closest_base_station.R"))
 
 # Features per location, that reqire knowing the detections that correspond to each location
 calculate_detection_based_features <- function(localizations_data, detections_data) {
   
   # Get the detections corresponding to each location
-  machted_detections <- match_detections_to_localizations(localizations_data, detections_data)
+  matched_detections <- match_detections_to_localizations(localizations_data, detections_data)
   
-  localizations_data_with_features <- calculate_SNR_features(machted_detections, localizations_data)
+  # Calculate the SNR features
+  localizations_data_with_features <- calculate_SNR_features(matched_detections, localizations_data)
+  
+  # Calculate the distance of each location from the closest receiver
+  localizations_data_with_features <- calculate_distance_to_closest_base_station(
+    matched_detections,
+    localizations_data_with_features)
   
   return(localizations_data_with_features)
   
