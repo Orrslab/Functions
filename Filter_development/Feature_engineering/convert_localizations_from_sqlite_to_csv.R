eefe49f6636033d8faa12e41ca66f18806334474
ntsur <tsur.neta@gmail.com>
2025-05-23 03:00:20 +0300

    added table name to the output file name

--- Filter_development/Feature_engineering/convert_localizations_from_sqlite_to_csv.R
+++ Filter_development/Feature_engineering/convert_localizations_from_sqlite_to_csv.R
@@ -2,15 +2,18 @@ source(file.path(getwd(), "convert_sqlite_table_to_csv.R"))
 
 # From where to take the sqlite file
 sqlite_folder <- "C:/Users/netat/Documents/Movement_Ecology/Filter_development/Feature_Engineering/Data_with_features"
-filename <- "CB_features_eng"
+filename <- "RW_features_eng"
+
+table_name <- "LOCALIZATIONS"
+# table_name <- "DETECTIONS"
 
 # Where to save the csv file
 output_csv_folder <- "C:/Users/netat/Documents/Movement_Ecology/Filter_development/Feature_Engineering/Data_with_features/csv_files"
 
 # Define input and output file paths
 sqlite_path <- file.path(sqlite_folder, paste0(filename, ".sqlite"))
-output_csv_path <- file.path(output_csv_folder, paste0(filename, ".csv"))
+output_csv_path <- file.path(output_csv_folder, paste0(filename, "_", table_name, ".csv"))
 
 convert_sqlite_table_to_csv(sqlite_path = sqlite_path,
-                            table_name = "LOCALIZATIONS",
+                            table_name = table_name,
                             output_csv_path = output_csv_path)
