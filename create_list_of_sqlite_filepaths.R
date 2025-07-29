db19eb5ab89248ab5cc80a175e0debe92a31fa71
ntsur <tsur.neta@gmail.com>
2025-07-29 23:51:07 +0300

    added sqlite extension

--- create_list_of_sqlite_filepaths.R
+++ create_list_of_sqlite_filepaths.R
@@ -52,11 +52,15 @@ create_list_of_sqlite_filepaths <- function(data_requests, folder_path_to_sqlite
     start_time = request$start_time
     end_time = request$end_time
     
-    filepaths[[length(filepaths) + 1]] <- create_sqlite_filepath(animal_name_code,
-                                                                 tag_numbers,
-                                                                 start_time,
-                                                                 end_time,
-                                                                 folder_path_to_sqlite_files)
+    file_path_plain <- create_sqlite_filepath(animal_name_code,
+                                              tag_numbers,
+                                              start_time,
+                                              end_time,
+                                              folder_path_to_sqlite_files)
+    
+    file_path_with_extension <- paste0(file_path_plain, ".sqlite")
+    
+    filepaths[[length(filepaths) + 1]] <- file_path_with_extension
     
   }
   return(filepaths)
