de19226b2731b5d9c38ea9e18f9e256b891b6296
ntsur <tsur.neta@gmail.com>
2025-09-08 17:12:09 +0300

    refactored script prepare_feature_data_for_random_forest to a function called main_clean_features_data, and added it to the atlasRF pipeline.

--- Filter_development/main_build_atlasRF_pipeline.R
+++ Filter_development/main_build_atlasRF_pipeline.R
@@ -10,12 +10,17 @@ source(file.path(getwd(), "Filter_development/config_build_atlasRF.R"))
 # Load the other functions that do each steps of building the atlasRF
 source(file.path(getwd(), "Filter_development/Labeled_DB_establishment/main_establish_labeled_database.R"))
 source(file.path(getwd(), "Filter_development/Feature_engineering/main_calculate_features.R"))
+source(file.path(getwd(), "Filter_development/Feature_engineering/main_clean_features_data.R"))
 
 # Run the selected steps based on their flags from the config
 if (config$workflow_steps_to_run$establish_labeled_db){
   main_establish_labeled_database(config)
 }
 
-if (config$workflow_steps_to_run$calculate_deatures) {
+if (config$workflow_steps_to_run$calculate_features) {
   main_calculate_features(config)
+}
+
+if (config$workflow_steps_to_run$clean_features_data) {
+  main_clean_features_data(config)
 }
