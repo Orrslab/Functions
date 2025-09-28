a8050f23a1a21b8bab9de1e7fcbf5d783afc4be7
ntsur <tsur.neta@gmail.com>
2025-09-28 03:44:57 +0300

    new wrapper for cleaning the atlasRF results (removing Outliers == NA)

--- Apply_atlasRF/wrapper_run_atlasRF.R
+++ Apply_atlasRF/wrapper_run_atlasRF.R
@@ -9,9 +9,9 @@ wrapper_run_atlasRF <- function(cleaned_data, config) {
   atlasRF_model <- readRDS(file.path(config$paths$folder_atlasRF_model, config$paths$filename_random_forest_model))
   
   # Run atlasRF
-  localization_data_with_outliers_indication <- run_atlasRF_on_localization_data(
+  localization_data_with_outlier_labels <- run_atlasRF_on_localization_data(
     cleaned_data, atlasRF_model, config$random_forest_settings$rf_prediction_threshold)
   
   
-  return(localization_data_with_outliers_indication)
+  return(localization_data_with_outlier_labels)
 }
